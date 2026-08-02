6e9da52d854a40ebd4f774d983916c21000932a6
Shacsim <Shacsim@mail.ru>
2026-08-03 00:41:21 +0500

    =

--- Task1.py
+++ Task1.py
@@ -1,1 +1,10 @@
-print("Salom dunyo!")
+class Mashina:
+    def __init__(self,model,rang):
+        self.model = model
+        self.rang = rang
+
+    def yurish(self):
+        print(f"{self.rang} rangli {self.model} yurmoqda. ")
+
+m1 = Mashina("Captiva", "Oq")
+m1.yurish()
